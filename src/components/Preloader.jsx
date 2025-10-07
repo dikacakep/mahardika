f2747a96f9a93f6e3fba52eeeead04bd34aeddcb
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-10-07 16:43:33 +0700

    Update Preloader.jsx

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -11,7 +11,7 @@ const Preloader = ({ onFinished }) => {
   const [fadeOut, setFadeOut] = useState(false);
   // 1. State baru untuk melacak status loading Spline
   const [isAssetLoaded, setIsAssetLoaded] = useState(false);
-  const fullText = "www.mahardika.com";
+  const fullText = "plantvsbrainrots.vercel.app";
 
   // 2. Fungsi yang akan dipanggil saat Spline selesai dimuat
   const handleAssetLoad = () => {
